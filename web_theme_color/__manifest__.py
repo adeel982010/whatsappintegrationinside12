e2f6976a1be94448c6c54d7fba8993bb980ab3b4
Sky <you@example.com>
2020-05-20 21:39:25 +0530

    dynamic theme changes

---
+++ web_theme_color/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Web Theme Color',
+    'category': 'General',
+    'summary': 'Web Theme Color',
+    'version': '1.0.0',
+    'author':  '',
+    'website': '',
+    'depends': ['base', 'web', 'web_widget_color'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
