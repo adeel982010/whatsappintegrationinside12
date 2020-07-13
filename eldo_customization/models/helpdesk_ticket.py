1ddde856d61d10c5c526d1ac4c784a93f8fa7b33
Dhruvil <dhruvil.gosai1993@gmail.com>
2020-07-13 17:58:38 +0100

    [SOLVED]Message will be created in message section in case of incoming mail!

--- eldo_customization/models/helpdesk_ticket.py
+++ eldo_customization/models/helpdesk_ticket.py
@@ -9,22 +9,12 @@ class HelpdeskTicket(models.Model):
 
     @api.model
     def message_new(self, msg, custom_values=None):
-        values = dict(custom_values or {}, partner_email=msg.get('from'), partner_id=msg.get('author_id'))
-        ticket = super(HelpdeskTicket, self).message_new(msg, custom_values=values)
-
-        partner_ids = [x for x in ticket._find_partner_from_emails(self._ticket_email_split(msg)) if x]
-        customer_ids = ticket._find_partner_from_emails(tools.email_split(values['partner_email']))
-        partner_ids += customer_ids
-
-        if customer_ids and not values.get('partner_id'):
-            ticket.partner_id = customer_ids[0]
-        if partner_ids:
-            ticket.message_subscribe(partner_ids)
-        ticket.update({'stage_id': self.env.ref('eldo_customization.stage_new_email').id})
-        return ticket
+        res = super(HelpdeskTicket, self).message_new(msg=msg, custom_values=custom_values)
+        res.update({'stage_id': self.env.ref('eldo_customization.stage_new_email').id})
+        return res
 
     @api.multi
-    def message_update(self, msg, update_vals):
+    def message_update(self, msg, update_vals=None):
         res = super(HelpdeskTicket, self).message_update(msg=msg, update_vals=update_vals)
         self.write({'stage_id': self.env.ref('eldo_customization.stage_new_email').id})
         return res
